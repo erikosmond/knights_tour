5bf3885d0a2713f1779600b879a4e2307d719c43
Erik Osmond <erikosmond@gmail.com>
2013-09-09 20:12:49 -0400

    solid working version of the algorythm

--- knights_tour.py
+++ knights_tour.py
@@ -77,7 +77,8 @@ class Verbose(object):
         if self.retrace_switch:
             print "Retracing to ", chess_piece.visited_positions[-1]
         if self.visited_positions_switch:
-            self.final_positions(chess_piece)
+            pass
+            #self.final_positions(chess_piece)
 
     def potential_OBOB(self, tour):
         if self.potential_OBOB_switch:
@@ -173,6 +174,7 @@ class Position(object):
         self.board = board
         self.verbosity = Verbose(verbosity)
         self.fits_on_board = self._check_board()
+        self.failures = []
         if self.fits_on_board:
             self.weight = self.board.get_weight(self.row, self.column)
         else:
@@ -196,6 +198,14 @@ class Position(object):
         if not 0 < self.column <= self.board.columns:
                 return False
         return True
+
+    def record_failed_position(self, position):
+        self.failures.append(position)
+
+    def check_failed_position(self, position):
+        if position in self.failures:
+            return True
+        return False
     
 class ChessPiece(object):
 
@@ -235,37 +245,48 @@ class ChessPiece(object):
     def set_position(self, position):
         self.verbosity.every_move(position)
         added = self.record_visited_position(position)
-        #self.record_failed_position(self.current_position, position)
+        #self.record_failed_position(self.current_position, position) #this was commented out, it duplicates in knight.retrace()
         self.current_position = position
         self.verbosity.board(self)
         return added
-        
+
+
+
+    ###modify this method to adjust the position instead
     def record_failed_position(self, old_position, new_position):
+        old_position.record_failed_position(new_position)
+        #self.verbosity.failed_position(old_position, failed_moves) #This needs to be changed to reflect the trials being in the position
+        """
         failed_moves = self.get_failed_moves(old_position)
-        self.verbosity.failed_position(old_position, failed_moves)
         failed_moves.add(new_position)
         self.trials[old_position.coordinate] = failed_moves
-
-    def retrace(self):
-        
+        """
+    def retrace(self):  ##create retrace objects so I don't have to manage the memory:)
+        """
+        A knight can inherit both a chess_piece and a retrace, or should I have them all be conneted via inheritence?
+        I need to rethink the way i'm doing retraces because this two steps forward one step back thing is
+        really confusing, and I'm not incorporating
+        the fact that that is happening enough into my thinking
+        It might explain why it was easy to go back one step, but not two steps and why the failed positions seemed to grow and weird times
+        a move should be its own object and a retrace should inherit it
+        """
+        #pre retrace - print current possition, all possible moves from that position, and all failed moves on the board
         failed_position = self.visited_positions.pop()
         try:
             previous_position = self.visited_positions[-1]
         except:
             self.verbosity.final_positions(self)
             raise GameError()
-            
-        self.record_failed_position(previous_position, failed_position) #this might not be necissary as this should have already been recorded, or I only use this, and get rid of setting failed positions when I move the knight in the first place to be a little less confusing
-#        failed_positions = self.get_failed_moves(previous_position)
-#        failed_positions.add(failed_position)
         self.set_position(previous_position)
         self.verbosity.retrace(self)
-        #should remove failed positions recursively from failed_position
         print "removing failed position for", previous_position
         self.remove_failed_positions(previous_position.coordinate)
+        self.record_failed_position(previous_position, failed_position) #this might not be necissary as this should have already been recorded, or I only use this, and get rid of setting failed positions when I move the knight in the first place to be a little less confusing
+        #post retrace - print current position, the failed position, all possible moves from the current position and all stored failed moves
+
         return previous_position
 
-    def remove_failed_positions(self, failed_position):
+    def remove_failed_positions(self, failed_position): #now that failed trials are held in the position, I should be able to get rid of this
         failed_positions = self.get_failed_moves(failed_position)
         print str(failed_positions), "are the failed positions for", str(failed_position)
         if failed_positions == set():
@@ -295,15 +316,19 @@ class ChessPiece(object):
         failed_positions = self.get_failed_moves(self.get_current_position())
 
 
-        print "current failed positions for ", position 
+        print "current failed positions for in _valid_position method", position 
         for i in failed_positions:
             print i
 
-
+        if self.current_position.check_failed_position(position) == True:
+            #if it's already a failed position, return False
+            return False
         
-        for i in failed_positions:
+        for i in failed_positions: #should be able to remove this check with code above
             if position == i:
                 return False
+
+            
         for i in self.visited_positions:
             if position == i:
                 return False        
