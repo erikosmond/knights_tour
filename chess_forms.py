76dcffb33707454c101168eeda60a27d9aad2a49
Erik Osmond <erikosmond@gmail.com>
2013-10-02 18:50:04 -0400

    changing form to raw html as opposed to Django

--- chess_forms.py
+++ chess_forms.py
@@ -1,10 +1,18 @@
 from django import forms
 
+size_choices = (("4","4"), ("5","5"), ("6","6"), ("7","7"), ("8","8"), ("9","9"), ("10","10"), ("11","11"), ("12","12"))
+start_choices = (("1","1"),("2","2"),("3","3"),("4","4"), ("5","5"), ("6","6"), ("7","7"), ("8","8"), ("9","9"), ("10","10"), ("11","11"), ("12","12"))
+
 class StartTourForm(forms.Form):
-    rows = forms.CharField()
-    columns = forms.CharField()
-    starting_row = forms.CharField()
-    starting_column = forms.CharField()
+    rows = forms.ChoiceField(choices=size_choices, initial="8")
+    columns = forms.ChoiceField(choices=size_choices, initial="8")
+    starting_row = forms.ChoiceField(choices=start_choices, initial="1")
+    starting_column = forms.ChoiceField(choices=start_choices, initial="1")
+    tour_type = forms.ChoiceField(widget=forms.RadioSelect, choices=((False,"Open"), (True,"Closed")))
     
     #username=forms.EmailField()
     #password=forms.CharField(widget=forms.PasswordInput())
+    
+#for fields that don't require django validation like email and password, maybe just hard code html inputs?
+
+    
--- views.py
+++ views.py
@@ -12,21 +12,24 @@ def index(request):
     #return HttpResponse("Hello, Notes")
     if request.method=="POST":
         print "Received POST"
-        form=StartTourForm(request.POST)
-        if form.is_valid():
+        form=request.POST#StartTourForm(request.POST)
+        if form:#.is_valid():
             print "Form is valid",
             # user registration or login code
             rows = int(request.POST.get("rows",None))
             columns = int(request.POST.get("columns",None))
             starting_row = request.POST.get("starting_row",None)
             starting_column = request.POST.get("starting_column",None)
-            closed = False #add this value from the form then use it here
+            closed = request.POST.get("tour_type",False)
+            if closed == "Closed":
+                closed = True
             #get speed and size from the user as well
             verbosity = 0
             if not rows or not columns or not starting_row or not starting_column:
-                return HttpResponse("Username or password not present")
+                return HttpResponse("One or more fields are missing")
             starting_location = str(starting_row) + '.' + str(starting_column)
             print "Starting Tour"
+            print closed
             try:
                 t = Tour(rows, columns, starting_location, verbosity, closed)
                 knight, count, board = t.run()
@@ -43,16 +46,24 @@ def index(request):
             table_width = r'"' + str(cell_s * columns + columns) + r'"px'
             table_height = r'"' + str(cell_s * rows + rows) + r'"px'
             template=loader.get_template("tour.html")
-            rc=RequestContext(request,{'cells':positions, "rows":rows, "columns":columns, "len_positions":len(str(positions))-1, "cell_size":cell_size, "table_width":table_width, "table_height":table_height})
+            rc=RequestContext(request,{'cells':positions, "rows":rows, "columns":columns,
+                                       "len_positions":len(str(positions))-1,
+                                       "cell_size":cell_size,
+                                       "table_width":table_width,
+                                       "table_height":table_height,
+                                       "count":count
+                                       })
             return HttpResponse(template.render(rc)) 
             
             #return redirect(dashboard)
         else:
             print "Form is not valid"
+            for error in form.errors:
+                print error
             template=loader.get_template("index.html")
-            rc=RequestContext(request,{'username':"Erik", "form":form})
+            rc=RequestContext(request,{'Error':'There was an error loading the page', "form":form})
             return HttpResponse(template.render(rc))            
     else:
         template=loader.get_template("index.html")
-        rc=RequestContext(request,{'username':"Erik", "form":StartTourForm()})
+        rc=RequestContext(request,{'username':"Erik"})#, "form":StartTourForm()}) #HtmlTourForm
         return HttpResponse(template.render(rc))
